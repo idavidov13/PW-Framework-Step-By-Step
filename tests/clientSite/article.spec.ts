1aa41ff1223ef7393e102521d56f0caa5b3cae8e
Ivan Davidov <ivan.davidov@primeholding.com>
2025-06-19 16:50:48 +0300

    revert

--- tests/clientSite/article.spec.ts
+++ tests/clientSite/article.spec.ts
@@ -37,7 +37,7 @@ test.describe('Verify Publish/Edit/Delete an Article', () => {
                 const responseBody = await response[1].json();
                 articleId = responseBody.article.slug;
             });
-            console.log('articleId', articleId);
+            
             await test.step('Verify Edit an Article', async () => {
                 await articlePage.navigateToEditArticlePage();
 
